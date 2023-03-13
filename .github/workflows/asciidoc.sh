1c16bd988f00f27f8a776f780f3591385cbcb728
Petr Kovar <pkovar@redhat.com>
2023-03-13 04:51:08 +0100

    Add homework project templates
    Add a Build PR workflow.
    List files in deployed dirs.

--- .github/workflows/asciidoc.sh
+++ .github/workflows/asciidoc.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 set -ex
 
-mkdir -p ./output
+mkdir -p ./output/homework-projects
 
 CURRENT_PATH=`pwd`
 
@@ -9,4 +9,12 @@ asciidoctor --verbose -D ${CURRENT_PATH}/output/ index.adoc
 
 asciidoctor-pdf --verbose -D ${CURRENT_PATH}/output/ -o technical-writing-course-brno.pdf index.adoc
 
-cp -t ${CURRENT_PATH}/output/ README.md MUNI-students-spring2023.pdf
+find -iname "*index*.adoc" -path "*/homework-projects/*" -type f -exec asciidoctor --verbose {} \;
+
+find -iname "*index*.adoc" -path "*/homework-projects/*" -type f -exec asciidoctor-pdf --verbose {} \;
+
+find -path "*/homework-projects/*" -type d -exec cp -r --verbose {} ${CURRENT_PATH}/output/homework-projects/ \;
+
+cp -r -t ${CURRENT_PATH}/output/ ${CURRENT_PATH}/slides/ ${CURRENT_PATH}/MUNI-students-spring2023.pdf
+
+cp -t ${CURRENT_PATH}/output/homework-projects/ ${CURRENT_PATH}/homework-projects/index.html
